be37d2823c98fe9a18c9f65e3311dc6fd7fdb931
JackHuynh2001 <9jhuynh@stpatrick.org>
2016-10-07 12:54:21 -0500

    OctoCat continued
    added slap function

--- octocats.js
+++ octocats.js
@@ -1,6 +1,14 @@
 //Write your constructor function below.
 //You should have 2 instance variables and 1 instance function included.
+function Octocat (n,a){
 
+this.name = n
+this.arms = a
+  }
+  this.slap = function(){
+    for(var a=0; a<this.arms;a++)
+}
+}
 
 
 
